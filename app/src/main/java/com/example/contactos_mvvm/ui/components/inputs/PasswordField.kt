0764daa8e8e666d3d341171d399a124391f8cf6b
JNMC0 <20223tn119@utez.edu.mx>
2025-10-23 15:41:02 -0600

    agregue la primera parte del composable en password

--- app/src/main/java/com/example/contactos_mvvm/ui/components/inputs/PasswordField.kt
+++ app/src/main/java/com/example/contactos_mvvm/ui/components/inputs/PasswordField.kt
@@ -1,2 +1,13 @@
 package com.example.contactos_mvvm.ui.components.inputs
 
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalFocusManager
+
+@Composable
+fun PasswordField(viewModel: LoginViewModel, label: String = "Contraseña") {
+    val focusManager = LocalFocusManager.current
+
+    OutlinedTextField(
+        value = viewModel.password.value,
+        onValueChange = { viewModel.password.value = it },
