9d732fa2023798bdd0d930e1caa168bf4c0e4585
JNMC0 <20223tn119@utez.edu.mx>
2025-10-23 15:42:39 -0600

    iniciamos el Userinput y creo el composable

--- app/src/main/java/com/example/contactos_mvvm/ui/components/inputs/UserInputField.kt
+++ app/src/main/java/com/example/contactos_mvvm/ui/components/inputs/UserInputField.kt
@@ -1,2 +1,11 @@
 package com.example.contactos_mvvm.ui.components.inputs
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun UserInputField(
+    viewModel: LoginViewModel,
+    label: String = "Usuario",
+    onNext: (() -> Unit)? = null
+)
+
