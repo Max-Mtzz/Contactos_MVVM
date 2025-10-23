f00629ad5a5d5d82373e4be419a17e72e1789873
JNMC0 <20223tn119@utez.edu.mx>
2025-10-23 15:38:48 -0600

    agregue e importe el primer Composable

--- app/src/main/java/com/example/contactos_mvvm/ui/components/inputs/NumberInputField.kt
+++ app/src/main/java/com/example/contactos_mvvm/ui/components/inputs/NumberInputField.kt
@@ -1,2 +1,9 @@
 package com.example.contactos_mvvm.ui.components.inputs
 
+import androidx.compose.runtime.Composable
+
+@Composable
+fun NumberInputField(
+    label: String,
+    state: androidx.compose.runtime.MutableState<String>
+)
