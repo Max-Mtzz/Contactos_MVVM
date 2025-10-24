dd2e4b22a9c7a73f52bc8e583b9f33f7c67eaf40
Sam6482 <20243ds141@utez.edu.mx>
2025-10-23 19:15:19 -0600

    Hice el navigation jeje

---
+++ app/src/main/java/com/example/contactos_mvvm/ui/Navigation.kt
@@ -0,0 +1,22 @@
+package com.example.contactos_mvvm.ui
+
+import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.contactos_mvvm.viewmodel.LoginViewModel
+
+@Composable
+fun Navigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "login") {
+        composable("login") {
+            val viewModel: LoginViewModel = viewModel() // instancia del ViewModel
+            LoginScreen(viewModel = viewModel, navController = navController)
+        }
+
+         }
+
+      }
