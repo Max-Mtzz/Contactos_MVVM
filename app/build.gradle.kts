feb1d83fa33b416460fe9411b6499766c6ecb638
Max-Mtzz <20243ds119@utez.edu.mx>
2025-10-23 19:51:32 -0600

    Hice el el screen del login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,7 +49,6 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-    implementation(libs.firebase.firestore.ktx)
     implementation(libs.androidx.navigation.compose)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
